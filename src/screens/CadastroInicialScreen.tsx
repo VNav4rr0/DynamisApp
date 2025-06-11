ade82d7190b7ff656ea92850a5db9ae2fcaf8253
Joaozin54P <peuzao.2018@gmail.com>
2025-06-10 23:50:39 -0300

    Tela De Cadastro Feita

--- src/screens/CadastroInicialScreen.tsx
+++ src/screens/CadastroInicialScreen.tsx
@@ -390,7 +390,7 @@ const styles = StyleSheet.create({
     fontSize: screenWidth * 0.04,
   },
   buttonContainer: {
-    paddingVertical: 20,
+    paddingVertical: 44,
     alignItems: 'center',
   },
   roundedButton: {
